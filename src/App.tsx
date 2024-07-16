8b964c5bf7a63ca347aded76da46d7d4bcc5b13e
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2024-07-16 18:20:03 +0200

    Config project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Button } from './components/button'
 import logo from './logo.svg'
 
 function App() {
@@ -8,13 +9,13 @@ function App() {
     <div className="flex flex-col gap-8 justify-center items-center h-screen text-white bg-gray-800">
       <img src={logo} className="w-[600px] animate-spin-slow" alt="logo" />
       <h1 className="text-6xl font-bold">Hello Vite + React + Tailwind!</h1>
-      <button
-        className="py-2 px-3 text-xl font-bold bg-sky-400 active:bg-sky-600 rounded-md transition-all hover:scale-105"
+      <Button
         type="button"
+        variant="destructive"
         onClick={() => setCount((count) => count + 1)}
       >
         Count is: {count}
-      </button>
+      </Button>
       <p>
         Edit <code className="p-1 bg-gray-600 rounded-md">App.tsx</code> and
         save to test HMR updates.
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { queryClient } from './config/react-query'
 import './index.css'
 
 //eslint-disable-next-line
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 )
