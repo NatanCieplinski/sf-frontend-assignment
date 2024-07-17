fc770f71c9d8fbb154a15fc823b8323a4a4c5f55
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2024-07-17 23:24:18 +0200

    Add responsiveness to login and register

--- src/features/Login.tsx
+++ src/features/Login.tsx
@@ -1,7 +1,15 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { z } from 'zod'
 import { Button } from '../components/button'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../components/card'
 import {
   Form,
   FormControl,
@@ -36,36 +44,55 @@ export const Login = () => {
   }
 
   return (
-    <div className="flex flex-col gap-8 justify-center items-center w-full h-screen">
+    <div className="flex flex-col gap-8 justify-center items-center p-2 w-full h-screen bg-black sm:p-0 xs:p-8">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input id="email" type="email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input id="password" type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Login</Button>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full sm:w-[380px]"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input id="email" type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input id="password" type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter className="flex flex-col">
+              <Button type="submit" variant="secondary" className="w-full">
+                Login
+              </Button>
+              <Button variant="link" asChild>
+                <Link to="/register" className="text-sm">
+                  Register
+                </Link>
+              </Button>
+            </CardFooter>
+          </Card>
         </form>
       </Form>
     </div>
